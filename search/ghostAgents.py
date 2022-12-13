9486558b01184b39ccfc51775ade41368f72f5e0
Eugene <eugene.it04@gmail.com>
2022-12-14 01:46:28 +0200

    Add directional ghost

--- search/ghostAgents.py
+++ search/ghostAgents.py
@@ -15,7 +15,6 @@
 from game import Agent
 from game import Actions
 from game import Directions
-import random
 from util import manhattanDistance
 import util
 
@@ -44,17 +43,11 @@ class RandomGhost( GhostAgent ):
 
 class DirectionalGhost( GhostAgent ):
     "A ghost that prefers to rush Pacman, or flee when scared."
-    def __init__( self, index, prob_attack=0.8, prob_scaredFlee=0.8 ):
-        self.index = index
-        self.prob_attack = prob_attack
-        self.prob_scaredFlee = prob_scaredFlee
 
     def getDistribution( self, state ):
-        # Read variables from state
-        ghostState = state.getGhostState( self.index )
         legalActions = state.getLegalActions( self.index )
         pos = state.getGhostPosition( self.index )
-        isScared = ghostState.scaredTimer > 0
+        isScared = state.getGhostState( self.index ).scaredTimer > 0
 
         speed = 1
         if isScared: speed = 0.5
@@ -63,19 +56,13 @@ class DirectionalGhost( GhostAgent ):
         newPositions = [( pos[0]+a[0], pos[1]+a[1] ) for a in actionVectors]
         pacmanPosition = state.getPacmanPosition()
 
-        # Select best actions given the state
         distancesToPacman = [manhattanDistance( pos, pacmanPosition ) for pos in newPositions]
         if isScared:
             bestScore = max( distancesToPacman )
-            bestProb = self.prob_scaredFlee
         else:
             bestScore = min( distancesToPacman )
-            bestProb = self.prob_attack
-        bestActions = [action for action, distance in zip( legalActions, distancesToPacman ) if distance == bestScore]
+        index = distancesToPacman.index(bestScore)
 
-        # Construct distribution
         dist = util.Counter()
-        for a in bestActions: dist[a] = bestProb / len(bestActions)
-        for a in legalActions: dist[a] += ( 1-bestProb ) / len(legalActions)
-        dist.normalize()
+        dist[legalActions[index]] = 1.0
         return dist
